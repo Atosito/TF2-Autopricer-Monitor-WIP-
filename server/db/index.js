fea1e018ba1de364e324a037b32c3b95e23f92a6
Tomas <gonzaltomas@gmail.com>
2020-09-01 13:36:05 -0300

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,15 @@
-const connect = (mongoose) => {
+const connect = async (mongoose) => {
     mongoose.set('useFindAndModify', false);
     mongoose.set('useUnifiedTopology', true);
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/TF2Autopricer', async err => {
-        if (err){
+    mongoose.connect('mongodb://localhost/TF2Autopricer', err => {
+        if (err) {
             console.log(err)
         }
         console.log('Connected to database correctly!')
+        return;
+        
     })
 };
 
