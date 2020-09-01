18bf75828267c3f3cfeb5bd8618c63bbcb66006a
Tomas <gonzaltomas@gmail.com>
2020-09-01 00:51:23 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,40 @@
 
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const port = normalizePort(process.env.PORT || '80');
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+const autopricer = require('./autopricer/app.js')
+
+const auto = new autopricer(io)
+
+
+async function start() {
+    try {
+        await auto.init();
+        auto.addItems();
+        server.listen(port);
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
+//
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
